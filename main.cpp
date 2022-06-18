fe4c5b4138e12270b023c0b23ef0d7a75e13d743
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-06-18 21:15:42 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 #include <string>
 #include <filesystem>
 
+int scan_dir(std::vector<std::string> &dir_vector)
+{
+    std::string input_file_line;
+    std::ifstream input_file;
+    std::string user_input;
+    for (int i = 0; i <= dir_vector.size() - 1; i++)
+    {   
+        std::cout << "[+] Opening file: " << dir_vector[i] << "\n\n";
+        input_file.open(dir_vector[i]);
+        while (std::getline(input_file, input_file_line))
+        {
+            //std::cout << input_file_line << "\n";
+            if (input_file_line.find("1") != std::string::npos)
+            {
+                std::cout << input_file_line << " END " << "\n\n";
+            }
+        }
+        input_file.close();
+    }
+    return 1;
+}
+
 int main()
 {
     std::cout << "=======================================" << "\n";
@@ -12,34 +34,38 @@ int main()
     std::cout << "- Current location of executable: " << std::filesystem::current_path() << "\n";
     std::cout << "=======================================" << "\n\n";
     std::vector<std::string> dir_vector;
-    std::string input_file_line;
     std::ifstream input_file;
     std::string user_input;
-    std::cout << "[>] Select directory to check:" << "\n";
-    std::getline(std::cin, user_input);
-    std::cout << "\n";
-    if (std::filesystem::is_directory(user_input) == 0)
+    std::cout << "Show current directoy files" << "\n";
+    for (const auto& entry : std::filesystem::directory_iterator(std::filesystem::current_path()))
     {
-        std::cout << "[!] Entered user input does not exist." << "\n";
-    }
-    else
-    {
-        // List all files within selected directory
-        for (const auto& entry : std::filesystem::directory_iterator(user_input))
-        std::cout << entry.path() << std::endl;
-    }
-    input_file.open(user_input);
-    while (std::getline(input_file, input_file_line))
-    {
-        //std::cout << input_file_line << "\n";
-        if (input_file_line.find("1") != std::string::npos)
+        if (entry.path().extension() == ".txt")
         {
-            std::cout << input_file_line << " END " << "\n\n";
+            std::cout << entry.path() << "\n";
+            dir_vector.push_back(entry.path().string());
         }
     }
-    input_file.close();
-    std::cout << "[!] END" << "\n";
-    std::cout << "[!] Exiting..." << "\n\n";
-    std::cin.get();
+    while (true)
+    {
+        std::cout << "[>] Select directory to check:" << "\n";
+        std::getline(std::cin, user_input);
+        std::cout << "\n";
+        if (std::filesystem::is_directory(user_input) == 0)
+        {
+            std::cout << "[!] Entered user input does not exist." << "\n";
+        }
+        else
+        {
+            // List all files within selected directory
+            for (const auto& entry : std::filesystem::directory_iterator(user_input))
+            {
+                std::cout << entry.path() << "\n";
+            }
+        }
+        scan_dir(dir_vector);
+        std::cout << "[!] END" << "\n";
+        std::cout << "[!] Exiting..." << "\n\n";
+        std::cin.get();
+    }
     return 0;
 }
